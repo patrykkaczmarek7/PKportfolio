9b84246d4d585c5f0fcbf975e13603aa468ec21b
Patryk Kaczmarek <patrykkaczmarek7@gmail.com>
2023-11-01 15:32:46 +0100

    update

--- src/components/About.js
+++ src/components/About.js
@@ -8,17 +8,19 @@ import js from '../images/About/js.png';
 import bootstrap from '../images/About/bootstrap.png';
 import angular from '../images/About/angular.png';
 import sass from '../images/About/sass.png';
-import tailwind from '../images/About/tailwind.png';
-import php from '../images/About/php.png';
-import mysql from '../images/About/mysql.png';
-import cplusplus from '../images/About/cplusplus.png';
-import nestjs from '../images/About/nestjs.png';
-import nodejs from '../images/About/nodejs.png';
-import expressjs from '../images/About/expressjs.png';
-import mongodb from '../images/About/mongodb.png';
-import photoshop from '../images/About/photoshop.png';
-import xd from '../images/About/xd.png';
-import corel from '../images/About/corel.png';
+import material from '../images/About/material.png';
+import rxjs from '../images/About/rxjs.png';
+import ts from '../images/About/ts.png';
+// import php from '../images/About/php.png';
+// import mysql from '../images/About/mysql.png';
+// import cplusplus from '../images/About/cplusplus.png';
+// import nestjs from '../images/About/nestjs.png';
+// import nodejs from '../images/About/nodejs.png';
+// import expressjs from '../images/About/expressjs.png';
+// import mongodb from '../images/About/mongodb.png';
+// import photoshop from '../images/About/photoshop.png';
+// import xd from '../images/About/xd.png';
+// import corel from '../images/About/corel.png';
 import useWindowPosition from '../hook/useWindowPosition';
 
 
@@ -37,12 +39,14 @@ export default function About() {
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={html}/>
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={css}/>
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={js}/> <br/>
-                            <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={bootstrap}/>
+                            <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={material}/>
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={angular}/>
-                            <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={sass}/> <br/>
-                            <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={tailwind}/>
+                            <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={ts}/> <br/>
+                            <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={bootstrap}/>
+                            <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={rxjs}/>
+                            <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={sass}/>
                         </div>
-                        <div className="text-center w-full"> <br/> <br className="md:hidden lg:hidden lx:hidden"/>
+                        {/* <div className="text-center w-full"> <br/> <br className="md:hidden lg:hidden lx:hidden"/>
                             <p className="font-bold pb-4 text-4xl">Backend</p>
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={php}/>
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={mysql}/>
@@ -57,7 +61,7 @@ export default function About() {
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={photoshop}/>
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={xd}/>
                             <img className="inline-block text-gray-700 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 text-center rounded-md bg-gray-100 px-4 py-2 m-2 h-16 md:h-32 lg:h-20 xl:h-24" src={corel}/>
-                        </div>  
+                        </div>   */}
                     </div>
                 </div>    
             </div>
